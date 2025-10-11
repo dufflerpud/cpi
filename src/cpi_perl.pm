8721b2df67a98a486aee557991f3f3acea9f160a
Christopher Caldwell <dufflerpud@yahoo.com>
2025-10-11 17:02:28 -0400

    Added src/cpi_perl

---
+++ src/cpi_perl.pm
@@ -0,0 +1,51 @@
+#!/usr/bin/perl -w
+########################################################################
+#@HDR@	$Id$
+#@HDR@		Copyright 2025 by
+#@HDR@		Christopher Caldwell/Brightsands
+#@HDR@		P.O. Box 401, Bailey Island, ME 04003
+#@HDR@		All Rights Reserved
+#@HDR@
+#@HDR@	This software comprises unpublished confidential information
+#@HDR@	of Brightsands and may not be used, copied or made available
+#@HDR@	to anyone, except in accordance with the license under which
+#@HDR@	it is furnished.
+########################################################################
+
+use strict;
+
+package cpi_perl;
+use Exporter;
+use AutoLoader;
+our @ISA = qw /Exporter/;
+#@ISA = qw( Exporter AutoLoader );
+##use vars qw ( @ISA @EXPORT );
+our @EXPORT_OK = qw( );
+our @EXPORT = qw( pretty_qw );
+use lib ".";
+
+#__END__
+1;
+
+#########################################################################
+#	Return a prettier qw from a list of items.			#
+#	Note that the first item will be something like "@foo=qw("	#
+#	and the last item will be something like ");"			#
+#########################################################################
+sub pretty_qw
+    {
+    my( @items ) = @_;
+    my @lines = ( shift(@items) );
+    my $end = pop( @items );
+    foreach my $item ( @items )
+	{
+	if( ( length($lines[$#lines])+length($item)+1 ) > 70 )
+	    { push( @lines, $item ); }
+	else
+	    { $lines[$#lines] .= " $item"; }
+	}
+    return join("\n ",@lines) . $end;
+    }
+
+1;
+
