3ff5e118d00b46957bc01b05154a04d71187a305
Christopher Caldwell <dufflerpud@yahoo.com>
2025-12-07 14:37:14 -0500

    Initial

---
+++ tests/cpi_send_file.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use lib "/usr/local/lib/perl";
+use cpi_file qw( fatal cleanup );
+use cpi_time qw( time_string );
+use cpi_arguments qw( parse_arguments );
+use cpi_send_file qw( sendmail );
+use cpi_vars;
+
+our @problems;
+our %ARGS;
+our @files;
+
+my $now_string = &time_string("%04d-%02d-%02d %02d:%02d:%02d");
+
+#########################################################################
+#	Print a usage message and die.					#
+#########################################################################
+sub usage
+    {
+    &fatal( @_,
+	"",
+	"Usage:  $cpi_vars::PROG { <argument> } file file file...",
+	"where <argument> is one or more of:",
+	"-to <address>",
+	"-from <address>",
+	"-subject <subject>");
+    }
+
+#########################################################################
+#	Main								#
+#########################################################################
+%ARGS = &parse_arguments(
+    {
+    #flags		=>	[ "delete", "yes", "ask_password" ],
+    switches=>
+	{
+	"from"		=>	"chris.interim\@gmail.com",
+	"to"		=>	"chris.interim\@gmail.com",
+	"subject"	=>	"Mail sent $now_string",
+	"message"	=>	"This is a test message."
+	},
+    non_switches	=>	\@files
+    } );
+
+&sendmail(
+    $ARGS{from},
+    $ARGS{dest},
+    $ARGS{subject},
+    $ARGS{message},
+    @files );
+&cleanup(0);
